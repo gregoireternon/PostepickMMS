a6d2e6fb50d2a45c3786bbe85685f8d72dea419d
Gregoire Ternon <gregoire.ternon@gmail.com>
2017-11-03 20:02:10 +0100

    Initial Commit with a working MMS image export to a folder, and a non blocking UI

---
+++ PostepickMMS/app/src/main/java/postepick/mms/ExportMMSTask.java
@@ -0,0 +1,178 @@
+package postepick.mms;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
+/**
+ * Created by Gregoire on 03/11/2017.
+ */
+
+public class ExportMMSTask extends AsyncTask<Void, Integer, Boolean> {
+
+    Context _context;
+
+    Button _button;
+
+
+    public ExportMMSTask(Context c, Button button){
+        _context = c;
+        _button = button;
+    }
+
+    public void launch(){
+        _button.setText(R.string.launch_button_launched);
+        this.execute();
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+
+        try {
+            Cursor c =  _context.getContentResolver().query(Uri.parse("content://mms/inbox"),null,null,null,null);
+            while(c.moveToNext()){
+                System.out.println("sms entry");
+                int mmsId = c.getInt(0);
+                String selectionPart = "mid=" + mmsId;
+                Uri uri = Uri.parse("content://mms/part");
+                Cursor cursor = _context.getContentResolver().query(uri, null,
+                        selectionPart, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String partId = cursor.getString(cursor.getColumnIndex("_id"));
+                        String type = cursor.getString(cursor.getColumnIndex("ct"));
+                        if ("text/plain".equals(type)) {
+                            String data = cursor.getString(cursor.getColumnIndex("_data"));
+                            String body;
+                            if (data != null) {
+                                // implementation of this method below
+                                body = getMmsText(partId);
+                            } else {
+                                body = cursor.getString(cursor.getColumnIndex("text"));
+                            }
+                        }
+                        if ("image/jpeg".equals(type) || "image/bmp".equals(type) ||
+                                "image/gif".equals(type) || "image/jpg".equals(type) ||
+                                "image/png".equals(type)) {
+                            getMmsImage(partId, type);
+                        }
+                    } while (cursor.moveToNext());
+                }
+            }
+            Log.i(null, "FINISHED");
+
+        }catch(Exception e){
+            Log.e(this.getClass().getName(),"Error exporting mms", e);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        _button.setText(R.string.launch_button);
+        super.onPostExecute(aBoolean);
+
+    }
+
+    private void getMmsImage(String _id, String type) {
+        Uri partURI = Uri.parse("content://mms/part/" + _id);
+        InputStream is = null;
+        Bitmap bitmap = null;
+        try {
+            boolean hasPermission = (ContextCompat.checkSelfPermission(_context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+            String secStore = System.getenv("SECONDARY_STORAGE");
+
+            is = _context.getContentResolver().openInputStream(partURI);
+            File eS = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File myFold = new File(eS + "/mmsFold");
+            if(!myFold.exists()){
+                if(!myFold.mkdirs()){
+                    Log.w(null, "pas de repertoire créé: ", null);
+                }
+            }
+            String fileExtension = ".jpg";
+            if ("image/bmp".equals(type)){
+                fileExtension = ".bmp";
+            }
+            else if("image/gif".equals(type)){
+                fileExtension = ".gif";
+            }
+            else if("image/png".equals(type)){
+                fileExtension = ".png";
+            }
+            String fileName=myFold.getAbsolutePath()+"/"+ UUID.randomUUID().toString()+fileExtension;
+            FileOutputStream fos = new FileOutputStream(new File(fileName));
+            try{
+                byte[] buffer = new byte[8 * 1024];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+            }catch(Exception e){
+
+            }finally{
+                try {
+                    is.close();
+                }catch(Exception e2){}
+                try {
+                    fos.close();
+                }catch(Exception e2){}
+            }
+            Log.i(null, "getMmsImage: file written:"+fileName);
+        } catch (IOException e) {
+            Log.e(null, "getMmsImage: ",e );
+        }
+        finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {}
+            }
+        }
+    }
+    private String getMmsText(String id)  {
+        Uri partURI = Uri.parse("content://mms/part/" + id);
+        InputStream is = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            is = _context.getContentResolver().openInputStream(partURI);
+            if (is != null) {
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader reader = new BufferedReader(isr);
+                String temp = reader.readLine();
+                while (temp != null) {
+                    sb.append(temp);
+                    temp = reader.readLine();
+                }
+            }
+        } catch (IOException e) {}
+        finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {}
+            }
+        }
+        return sb.toString();
+    }
+
+}
---
+++ PostepickMMS/app/src/main/java/postepick/mms/MainActivity.java
@@ -0,0 +1,81 @@
+package postepick.mms;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final Button extractButton = (Button) findViewById(R.id.LaunchExtract);
+        extractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExportMMSTask exporter = new ExportMMSTask(MainActivity.this,extractButton);
+                exporter.launch();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
